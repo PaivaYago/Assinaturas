933f72ecf7937da075f56fede29cc4f2ec57febd
Yago Martins <41396854+PaivaYago@users.noreply.github.com>
2025-03-24 18:21:48 -0300

    Commit inicial
    Commit inicial

---
+++ Assinaturas/src/test/java/com/globo/assinaturas/Assinaturas/AssinaturasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globo.assinaturas.Assinaturas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssinaturasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
